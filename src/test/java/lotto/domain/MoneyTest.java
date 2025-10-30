2694388a1a9207b1f272b612ca18b5bf993939ca
inseong <litrod1733@naver.com>
2025-10-30 12:49:30 +0900

    test(MoneyTest): Money 테스트 추가 (금액 유효성 및 toTicketCount)

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,4 +1,29 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.Test;
+import  org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MoneyTest {
+
+	@Test
+	@DisplayName("양수이면서 1000원 단위인 금액은 생성 성공")
+	void validMoney() {
+		assertDoesNotThrow(() -> Money.of(8000));
+	}
+
+	@Test
+	@DisplayName("0원 또는 음수일 때 예외 발생")
+	void invalidMoneyNegativeOrZero() {
+		assertThrows(IllegalArgumentException.class, () -> Money.of(0));
+		assertThrows(IllegalArgumentException.class, () -> Money.of(-1000));
+	}
+
+	@Test
+	@DisplayName("toTicketCount()는 구입 금액을 1000으로 나눈 개수를 반환")
+	void toTicketCount() {
+		Money money = Money.of(8000);
+		assertEquals(8, money.toTicketCount());
+	}
 }
