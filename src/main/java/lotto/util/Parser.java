788366af4c148db5259c9b95b33a52ce7c2117d9
inseong <litrod1733@naver.com>
2025-11-03 08:20:20 +0900

    feat(Parser): 입력 문자열을 정수 리스트로 변환하는 유틸 클래스 추가

--- src/main/java/lotto/util/Parser.java
+++ src/main/java/lotto/util/Parser.java
@@ -1,4 +1,16 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Parser {
+
+	public static List<Integer> parseInts(String input) {
+		try {
+			return Arrays.stream(input.split(",")).map(String::trim).map(Integer::parseInt).collect(Collectors.toList());
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("[ERROR] 올바른 숫자 형식이 아닙니다.");
+		}
+	}
 }
