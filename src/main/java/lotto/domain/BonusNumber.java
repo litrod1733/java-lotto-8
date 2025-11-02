2be7a91f0c3637246b931ff35aa9081c1cae9a9c
inseong <litrod1733@naver.com>
2025-11-03 08:17:23 +0900

    feat(BonusNumber: 보너스 번호 중복 및 범위 검증 로직 추가

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,4 +1,24 @@
 package lotto.domain;
 
 public class BonusNumber {
+	private final int number;
+
+	public BonusNumber(int number, WinningNumbers winningNumbers) {
+		validate(number, winningNumbers);
+		this.number = number;
+	}
+
+	private void validate(int number, WinningNumbers winningNumbers) {
+		if (number <1 || number > 45) {
+			throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이여야 합니다.");
+		}
+
+		if (winningNumbers.contains(number)) {
+			throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+		}
+	}
+
+	public int getNumber() {
+		return number;
+	}
 }
