e58b840686bac7577d9dd952a513f487f212fa10
inseong <litrod1733@naver.com>
2025-11-03 12:37:06 +0900

    feat: 로또 애플리케이션 실행 흐름 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,29 @@
 package lotto.controller;
 
+import lotto.domain.*;
+import lotto.service.*;
+import lotto.view.*;
+
 public class LottoController {
+	private final LottoService lottoService = new LottoService();
+	private final LottoResultService resultService = new LottoResultService();
+
+	public void run() {
+		Money money = InputView.readPurchaseAmount();
+
+		Tickets tickets = lottoService.purchase(money);
+		OutputView.printPurchasedTicket(tickets);
+
+		WinningNumbers winningNumbers = InputView.readWinningNumbers();
+		BonusNumber bonusNumber = InputView.readBonusNumber(winningNumbers);
+
+		LottoResult result = resultService.calculateResult(
+			tickets.values(),
+			winningNumbers,
+			bonusNumber
+		);
+
+		double profitRate = result.calculateProfitRate(money);
+		OutputView.printStatistics(result, profitRate);
+	}
 }
