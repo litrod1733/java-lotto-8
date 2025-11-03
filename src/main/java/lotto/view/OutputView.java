fe5975f93c1ac334b1fea87fc84426604cec831e
inseong <litrod1733@naver.com>
2025-11-03 09:23:01 +0900

    feat(OutputView): 당첨 통계 및 수익률 출력 뷰 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,34 @@
 package lotto.view;
 
+import lotto.domain.LottoResult;
+import lotto.domain.Rank;
+
 public class OutputView {
+
+	public static void printStatistics(LottoResult result, double profitRate) {
+		System.out.println("당첨 통계");
+		System.out.println("---------");
+		result.getResults().forEach((rank, count) -> {
+			if (rank == Rank.MISS) return;
+
+			printRankResult(rank, count);
+		});
+
+		System.out.printf("총 수익률은 %.2f%%입니다.%n", profitRate);
+	}
+
+	private static void printRankResult(Rank rank, int count) {
+		if (rank == Rank.SECOND) {
+			System.out.printf("%d개 일치, 보너스 볼 일치 (%d원) - %d개%n",
+				rank.getMatchCount(),
+				rank.getPrize(),
+				count);
+			return;
+		}
+
+		System.out.printf("%d개 일치 (%d원) - %d개%n",
+			rank.getMatchCount(),
+			rank.getPrize(),
+			count);
+	}
 }
